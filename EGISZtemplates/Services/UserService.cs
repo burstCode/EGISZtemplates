3fddd185f6922c8a8e0450eabc7ca72f843adcf0
burstCode <yourewelcomex@gmail.com>
2024-08-03 00:47:18 +0300

    Хеширование паролей #3

--- EGISZtemplates/Services/UserService.cs
+++ EGISZtemplates/Services/UserService.cs
@@ -1,6 +1,8 @@
 ﻿using EGISZtemplates.Data;
 using EGISZtemplates.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace EGISZtemplates.Services
@@ -16,8 +18,24 @@ namespace EGISZtemplates.Services
 
         public async Task<User> AuthenticateAsync(string email, string password)
         {
-            var user = await _context.Users.SingleOrDefaultAsync(u => u.Email == email && u.Password == password);
+            var hashedPassword = HashPassword(password);
+            var user = await _context.Users.SingleOrDefaultAsync(u => u.Email == email && u.Password == hashedPassword);
             return user;
+        }
+
+        // Переместить в отдельный класс
+        public static string HashPassword(string password)
+        {
+            using (SHA512 sha512 = SHA512.Create())
+            {
+                byte[] bytes = sha512.ComputeHash(Encoding.UTF8.GetBytes(password));
+                StringBuilder builder = new StringBuilder();
+                foreach (byte b in bytes)
+                {
+                    builder.Append(b.ToString("x2"));
+                }
+                return builder.ToString();
+            }
         }
 
         /*
